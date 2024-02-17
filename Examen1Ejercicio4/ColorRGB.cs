3da088afbf2c1c66729fb82559f45e293ca5bc01
salasjc <salasjc@HRD6402574>
2024-02-17 05:04:16 -0600

    Add project files.

---
+++ Examen1Ejercicio4/ColorRGB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1Ejercicio4
+{
+     class ColorRGB
+    {
+         int rojo;
+         int verde;
+         int azul;
+
+
+        //constructores
+        public int Rojo
+        {
+            get { return rojo; }
+            set { rojo = value; }
+        }
+
+        public int Verde
+        {
+            get { return verde; }
+            set { verde = value; }
+        }
+
+        public int Azul
+        {
+            get { return azul; }
+            set { azul = value; }
+        }
+
+        //este metodo calcula la escala de grises con la formula NTSC
+        public double CalcularEscalaGrises()
+        {
+            return 0.299 * Rojo + 0.587 * Verde + 0.114 * Azul;
+        }
+    }
+}
---
+++ Examen1Ejercicio4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Examen1Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+             //solicita al usuario  la cantidad de color. convierte una cadena de texto a numero entero
+
+            Console.WriteLine("Ingrese la cantidad de rojo (0-255):");
+            int rojo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad de verde (0-255):");
+            int verde = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad de azul (0-255):");
+            int azul = Convert.ToInt32(Console.ReadLine());
+
+            // Crea un nuevo objeto de la clase ColorRGB, ademas que asignan los valores de las cantidades de rojo, verde y azul que el usuario ingresó a las propiedades correspondientes del objeto color
+            ColorRGB color = new ColorRGB();
+            color.Rojo = rojo;
+            color.Verde = verde;
+            color.Azul = azul;
+
+            //Llamamos al método CalcularEscalaGrises del objeto color para calcular el valor en escala de grises del color utilizando la fórmula de la NTSC. El resultado se almacena en la variable escalaGrises.
+            double escalaGrises = color.CalcularEscalaGrises();
+
+            // muestra en consola el valor en escala de los grises
+            Console.WriteLine($"El valor en escala de grises es: {escalaGrises}");
+        }
+    }
+}
